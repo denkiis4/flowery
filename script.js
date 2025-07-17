4480fc5b49e5cf2c6bdc019cac55bb74d2114fa9
denkiis4 <cryst4li@yandex.ru>
2025-07-17 19:10:46 +0300

    Update and rename flowery to script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Плавная прокрутка при клике на навигацию
+document.querySelectorAll('.nav a').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
